6adc22bdf138004e72b3f8868b2742fb81154311
Teears <2734189761@qq.com>
2021-01-30 19:32:07 +0800

    学生历史记录chart v1.5

--- we/pages/stu_history/stu_history.js
+++ we/pages/stu_history/stu_history.js
@@ -1,18 +1,102 @@
 // pages/stu_history/stu_history.js
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    surpassOption:'',
+    finishRateOption:'',
+    detailOption:'',
+    total:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const that = this
+    wx.request({
+      method:"POST",
+      url: app.globalData.host+'/stu/historyInit',
+      header:{
+        "content-type":"application/x-www-form-urlencoded"
+      },
+      timeout:10000,
+      data:{
+        token:''
+      },
+      success:function(res){
+        that.setData({
+          total:res.data.total,
+          surpassOption:{
+            series:[{
+              name:'超过同伴',
+              type:'pie',
+              radius:['65%','85%'],
+              label:{position:'center'},
+              data:[{
+                value:res.data.surpass,name:'超过同伴\n'+res.data.surpass+'%',
+                itemStyle:{
+                  color:'#66CCFF',
+                  borderRadius: 10,
+                }},{
+                  value:100-res.data.surpass,
+                  itemStyle:{color:'#EFEFEF'}
+                }]
+            }]
+          },
+          finishRateOption:{
+            series:[{
+              name:'到勤率',
+              type:'pie',
+              radius:['70%','85%'],
+              label:{position:'center'},
+              data:[
+                {value:res.data.finishRate,name:'到勤率\n'+res.data.finishRate+'%',itemStyle:{color:'#66CC99'}},
+                {value:100-res.data.finishRate,itemStyle:{color:'#EFEFEF'}}
+              ]
+            }]
+          },
+          detailOption:{
+            xAxis: {
+              type: 'value',
+              show: false,
+            },
+            yAxis: {
+              type: 'category',
+              show:false,
+              data:['到勤','迟到/早退','缺席','请假']
+            },
+            series:[{
+              name:'detail',
+              type:'bar',
+              data:[{
+                value:res.data.finished,
+                itemStyle:{color:'#99CC33'}
+              },{
+                value:res.data.late,
+                itemStyle:{color:'#FF9966'}
+              },{
+                value:res.data.absent,
+                itemStyle:{color:'#CC3333'}
+              },{
+                value:res.data.dayoff,
+                itemStyle:{color:'#666666'}
+              }]
+            }]
+          }
 
+        })//setData
+      },
+      fail:function(){
+        wx.showToast({
+          title: '网络繁忙',
+          icon:"none",
+          duration: 2000
+        })
+      }
+    })
   },
 
   /**
@@ -27,11 +111,11 @@ Page({
    */
   onShow: function () {
     if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 0
-        })
-      }
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      })
+    }
   },
 
   /**
