f82e6d3835cc838523149223c4864884f87db23d
Teears <2734189761@qq.com>
2021-02-06 16:18:03 +0800

    完成学生实验室

--- we/stu/pages/stu_shiyanshi/stu_shiyanshi.js
+++ we/stu/pages/stu_shiyanshi/stu_shiyanshi.js
@@ -5,62 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tab:[{
+      tabTitle:"简介",
+      tabContent:'<center><h4>xxxx实验室介绍</h4></center><br><p>&emsp;&emsp;xxx实验室成立于2016年，隶属于四川师范大学计算机科学学院，xxx配备有先进的xx设备，可以容纳20位学生行xx实验，这是乱写的乱写的乱写的乱写为实验人员提供xxx保障。</p><p>&emsp;&emsp;这是第二段内容我们试试能否用HTML解析<img src="https://img.yzcdn.cn/vant/cat.jpeg" width="100%"/></p><p>&nbsp;&nbsp;&nbsp;&nbsp;负责人：张三</p><p>&nbsp;&nbsp;&nbsp;&nbsp;联系方式：18712344321</p>'
+    },{
+      tabTitle:"管理规定",
+      tabContent:"xxxxxxxxxxxxx保障用电安全，xxxxxxxxxxxxxxxxxxx不能携带危险物品,xxxxxxxxxxxx禁止携带有气味的食物进入xxxx。xxxxxxxxxxxxx在微信小程序进行签到xxxxxxxx"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    const that = this
+    wx.showLoading({
+      title: '加载中...',
+    })
+    setTimeout(function(){
+      wx.hideLoading()
+    },10000)
+    wx.request({
+      url: app.globalData.host + '/stu/shiyanshi',
+        method: "GET",
+        "header": {
+          "content-type": "application/json; charset=utf-8",
+          "token": ""
+        },
+        timeout: 10000,
+        success: function (res) {
+          that.setData({
+            tab:res.tab
+          })
+        },
+        fail:function(){
+          console.log("网络繁忙")
+        },
+        complete:function(){
+          wx.hideLoading()
+        }
+    })
   }
 })
