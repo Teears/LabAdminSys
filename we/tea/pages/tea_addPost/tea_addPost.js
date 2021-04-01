e8d6ba3106b1f91734f26b41b20f36ef5a0bf435
Teears <2734189761@qq.com>
2021-04-01 21:14:26 +0800

    教师个人信息，查看学生信息未完成

--- we/tea/pages/tea_addPost/tea_addPost.js
+++ we/tea/pages/tea_addPost/tea_addPost.js
@@ -1,66 +1,92 @@
 // tea/pages/tea_addPost/tea_addPost.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    labIdList:[],
+    title:"",
+    contentLength:0,
+    content:"",
+    checkBox: []
+  },
 
+  onChangeCheckbox(e) {
+    this.setData({
+      checkBox: e.detail,
+    });
+  },
+  changeTitle(e){
+    this.setData({
+      title: e.detail
+    })
+  },
+  changeContent(e){
+    this.setData({
+      content: e.detail,
+      contentLength: e.detail.length
+    })
+  },
+
+  submit:function(){
+    const that = this
+    console.log(that.data)
+    wx.showLoading({
+      title: '请稍后...',
+    })
+    wx.request({
+      method: "POST",
+      url: app.globalData.host + '/tea/addPost',
+      header: {
+        "content-type": "application/x-www-form-urlencoded",
+        "token": wx.getStorageSync('token')
+      },
+      data: {
+        title:that.data.title,
+        content:that.data.content,
+        labIdList: that.data.checkBox
+      },
+      timeout: 10000,
+      success: function (res) {
+        wx.showToast({
+          title: '已发布',
+          duration: 2000
+        })
+      },
+      fail:function(){
+        wx.showToast({
+          title: '网络繁忙',
+          icon: "none",
+          duration: 2000
+        })
+      },
+      complete:function(){
+        wx.hideLoading()
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    const that = this
+    wx.request({
+      url: app.globalData.host + '/tea/getLabList',
+      method: "GET",
+      "header": {
+        "content-type": "application/json; charset=utf-8",
+        "token": wx.getStorageSync('token')
+      },
+      timeout: 10000,
+      success: function (res) {
+        res = res.data
+        that.setData({
+          labIdList:res.data
+        })
+      }
+    })
   }
 })
--- we/tea/pages/tea_post/tea_post.js
+++ we/tea/pages/tea_post/tea_post.js
@@ -1,66 +1,73 @@
 // tea/pages/tea_post/tea_post.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    queshengMark: 0,
+    queshengText: "暂时未发布任何消息",
+    list: [],
+    dialogShow: false,
+    index:''
+  },
 
+  showPopup(e){
+    var index = e.currentTarget.dataset.index
+    this.setData({
+      index:index,
+      dialogShow: true
+    })
+  },
+
+  tapDialogButton(e) {
+    this.setData({
+      dialogShow: false
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const that = this
+    this.getMessageList().then(res=>{
+      if(res==0){
+        that.setData({
+          queshengMark:1
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getMessageList: function () {
+    const that = this
+    var promise = new Promise((resolve, reject) => {
+      wx.request({
+        url: app.globalData.host + '/tea/getPost',
+        method: "GET",
+        "header": {
+          "content-type": "application/json; charset=utf-8",
+          "token": wx.getStorageSync('token')
+        },
+        timeout: 10000,
+        success: function (res) {
+          res = res.data
+          that.setData({
+            list: res.data,
+          })
+          if(res.data == null || res.data == undefined){
+            resolve(0)
+          }else{
+            resolve(res.data.length)
+          }
+        },
+        fail: function () {
+          reject()
+        }
+      })
+    })
+    return promise
   }
 })
